44ba12be32359e9eab5ab5fd278cb6230af64d45
nemesisper@gmail.com <yirunzhou@outlook.com>
2018-06-07 09:47:30 +0800

    make tabEl[1] active

--- popup.js
+++ popup.js
@@ -4,24 +4,43 @@ document.addEventListener('DOMContentLoaded', function () {
   let tabs = chrome.extension.getBackgroundPage().tabs;
   
   let tabListEl = document.getElementById("tabList");
+  let activeTabEl = null;
+
 
   function populateTabs(){
     for(let i = tabs.length-1; i >= 0; i--){
       let tab = tabs[i];
 
       let tabEl = document.createElement("li");
+      tabEl.classList.add("tab");
       tabEl.dataset.id = tab.tabId;
 
-      let tabTitleEl = document.createElement("p")
+      let tabTitleEl = document.createElement("p");
       tabTitleEl.innerHTML = tab.title;
 
       tabEl.appendChild(tabTitleEl);
       tabListEl.appendChild(tabEl);
     }
+
+    activeTabEl = document.querySelectorAll(".tab")[1];
+    activeTabEl.classList.add("active");
   }
 
   populateTabs();
 
-  
+  function moveDown(){
+
+  }
+
+  chrome.commands.onCommand.addListener(function(command){
+    switch (command){
+      case "down":
+        console.log(command);
+        moveDown();
+        break;
+      default:
+        break;
+    }
+  })
   
 }, false);
